912057f09b8a2dde47a8eb3ffa9f1260e2c200cc
jobiols <jorge.obiols@gmail.com>
2020-07-23 12:47:11 -0300

    [ADD] modulo stock

--- ingeleco_default/__manifest__.py
+++ ingeleco_default/__manifest__.py
@@ -68,6 +68,7 @@
         'https://github.com/ingadhoc/product.git',
         'https://github.com/ingadhoc/account-invoicing.git',
         'https://github.com/ingadhoc/argentina-sale.git',
+        'https://github.com/ingadhoc/stock.git',
 
         'https://github.com/oca/partner-contact.git',
         'https://github.com/oca/web.git',
